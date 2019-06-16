829255628e2cab11bfc0be6c58768c393312a247
Evstropov Evgenii <EJohnF@users.noreply.github.com>
2019-06-16 20:49:08 +0200

    add extra data to reservation list

--- src/agenda/reservation-list/index.js
+++ src/agenda/reservation-list/index.js
@@ -206,6 +206,7 @@ class ReactComp extends Component {
         refreshControl={this.props.refreshControl}
         refreshing={this.props.refreshing || false}
         onRefresh={this.props.onRefresh}
+        extraData={this.props.extraData}
       />
     );
   }
